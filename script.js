1f4d920e0cbcefb95153370a570cdc43800681ed
marcoscya <markus080904@gmail.com>
2025-03-09 15:47:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let delay = 500; // Tiempo inicial del delay
+
+    // Seleccionar solo los elementos que ya tienen el atributo data-aos
+    document.querySelectorAll("[data-aos]").forEach((element) => {
+        element.setAttribute("data-aos-delay", delay);
+        delay += 100; // Aumenta el delay en 200ms para cada elemento
+    });
+
+    // Inicializar AOS
+    AOS.init({
+        once: false,  
+        mirror: true, 
+    });
+
+    // Forzar actualización de AOS en cada recarga rápida
+    setTimeout(() => {
+        AOS.refresh();
+    }, 500);
+
+    // Actualizar AOS al hacer scroll
+    document.addEventListener("scroll", () => {
+        AOS.refresh();
+    });
+
+    // Volver al inicio al recargar la página
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    };
+});
+
 function moveSlider() {
     let slider = document.getElementById("slider");
     let children = [...slider.children]; // Convertimos en un array
